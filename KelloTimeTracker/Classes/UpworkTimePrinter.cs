5a1efa6030e47eb0eac3537b3feae9a952aa3e5c
saaratrix <saaratrix@gmail.com>
2025-11-09 01:55:02 +0200

    Upgrade project to .NET 9 and allow inputting multiple weeks at once.

--- KelloTimeTracker/Classes/UpworkTimePrinter.cs
+++ KelloTimeTracker/Classes/UpworkTimePrinter.cs
@@ -12,91 +12,95 @@ namespace KelloTimetracker.Classes
         /// <summary>
         /// Takes a folder path in the form of "u {folder}"
         /// </summary>
-        /// <param name="a_folder"></param>
-        public static void PrintTimes(string a_folder)
+        /// <param name="foldersWithCommand"></param>
+        public static void PrintTimes(string foldersWithCommand)
         {     
-            if (a_folder.Length <= 2)
+            if (foldersWithCommand.Length <= 2)
             {
                 Program.WriteLineMessage("expected input: 'u folder'", ConsoleColor.Red);
                 return;
             }
-            
-            // Remove the "u " part.
-            string folder = a_folder.Substring(2);                                
 
             try
             {
-                IEnumerable<string> files = Directory.EnumerateFiles(folder);
+                
+                // IEnumerable<string> files = Directory.EnumerateFiles(folder);
+                var filesWithFolder = GetFiles(foldersWithCommand);
 
                 UpworkTimeFixer timeFixer = new UpworkTimeFixer();
-                double totalTime = 0;                    
+                double totalTime = 0;
 
-                foreach (string filename in files)
+                foreach (var (folder, files) in filesWithFolder)
                 {
-                    string date = filename.Substring(folder.Length + 1).Split('.')[0];
-
-                    Program.WriteLineMessage("Date: " + date, ConsoleColor.Yellow);  
-
-                    using (StreamReader sr = new StreamReader(filename))
+                    foreach (string filename in files)
                     {
-                        DateTime start;
-                        TimeSpan duration;
-                        double dailyTotal = 0;
-                        double restTotal = 0;
-                            
+                        string date = filename.Substring(folder.Length + 1).Split('.')[0];
 
-                        List<UpworkTimeEntry> entries = new List<UpworkTimeEntry>();
+                        Program.WriteLineMessage("Date: " + date, ConsoleColor.Yellow);
 
-                        while (!sr.EndOfStream)
+                        using (StreamReader sr = new StreamReader(filename))
                         {
-                            string line = sr.ReadLine();
+                            DateTime start;
+                            TimeSpan duration;
+                            double dailyTotal = 0;
+                            double restTotal = 0;
 
-                            if (line.StartsWith("start"))
+
+                            List<UpworkTimeEntry> entries = new List<UpworkTimeEntry>();
+
+                            while (!sr.EndOfStream)
                             {
-                                string startStr = line.Split(' ')[1];
-                                start = DateTime.Parse(startStr);
-                                // Read next line
-                                duration = TimeSpan.Parse(sr.ReadLine().Split(' ')[1]);
+                                string line = sr.ReadLine();
 
-                                UpworkTimeEntry entry = new UpworkTimeEntry(start, duration);
-                                entries.Add(entry);                                                                            
-                            }                                
-                        }
+                                if (line.StartsWith("start"))
+                                {
+                                    string startStr = line.Split(' ')[1];
+                                    start = DateTime.Parse(startStr);
+                                    // Read next line
+                                    duration = TimeSpan.Parse(sr.ReadLine().Split(' ')[1]);
 
-                        if (entries.Count > 0)
-                        {
-                            // BUG if 1 entry has less than 10 minutes work then the totalRest will be fucked
-                            entries = timeFixer.FixEntries(entries);                                
-
-                            foreach (UpworkTimeEntry entry in entries)
-                            {
-                                entry.PrintTime();
-                                dailyTotal += entry.DurationSeconds;                                    
+                                    UpworkTimeEntry entry = new UpworkTimeEntry(start, duration);
+                                    entries.Add(entry);
+                                }
                             }
 
-                            restTotal = timeFixer.RestSeconds;                               
-                                                                                      
-                            int hours = (int)(dailyTotal / 3600);
-                            int minutes = (int)((dailyTotal - (hours * 3600)) / 60);
+                            if (entries.Count > 0)
+                            {
+                                // BUG if 1 entry has less than 10 minutes work then the totalRest will be fucked
+                                entries = timeFixer.FixEntries(entries);
 
-                            Program.WriteLineMessage("Daily total: " + hours + "h " + minutes + "m", ConsoleColor.Cyan);
+                                foreach (UpworkTimeEntry entry in entries)
+                                {
+                                    entry.PrintTime();
+                                    dailyTotal += entry.DurationSeconds;
+                                }
 
-                            hours = (int)(restTotal / 3600);
-                            minutes = (int)((restTotal - (hours * 3600)) / 60);
+                                restTotal = timeFixer.RestSeconds;
 
-                            Program.WriteLineMessage("Daily overflow:" + hours + "h " + minutes + "m", ConsoleColor.Red);
+                                int hours = (int)(dailyTotal / 3600);
+                                int minutes = (int)((dailyTotal - (hours * 3600)) / 60);
 
-                            totalTime += dailyTotal;
+                                Program.WriteLineMessage("Daily total: " + hours + "h " + minutes + "m",
+                                    ConsoleColor.Cyan);
 
-                            Console.WriteLine();
+                                hours = (int)(restTotal / 3600);
+                                minutes = (int)((restTotal - (hours * 3600)) / 60);
+
+                                Program.WriteLineMessage("Daily overflow:" + hours + "h " + minutes + "m",
+                                    ConsoleColor.Red);
+
+                                totalTime += dailyTotal;
+
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("NO ENTRIES FOUND");
+                            }
                         }
-                        else
-                        {
-                            Console.WriteLine("NO ENTRIES FOUND");
-                        }                            
-                    }                        
+                    }
                 }
-                                        
+
                 int totalHours = (int)(totalTime / 3600);
                 int totalMinutes = (int)((totalTime - (totalHours * 3600)) / 60);
                     
@@ -111,6 +115,22 @@ namespace KelloTimetracker.Classes
             {
                 Program.WriteLineMessage("Unexpected error: " + e.Message, ConsoleColor.Red);
             }               
+        }
+
+        private static List<(string week, List<string> files)> GetFiles(string command)
+        {
+            // Remove the "u " part.
+            string folders = command.Substring(2);   
+            var parts = folders.Split(' ');
+
+            var output = new List<(string week, List<string> files)>();
+            foreach (var part in parts)
+            {
+                var files = Directory.EnumerateFiles(part).ToList();
+                output.Add((part, files));
+            }
+
+            return output;
         }
     }
 }
